ca2d202538d15d137726aeac7b6d51616733a15c
Maciej Karpovic <maciejkrp@gmail.com>
2023-12-11 19:24:58 +0100

    fix header PMB for screenreaders

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var dropdown = document.querySelector(".header__item--has-dropdown");
+  var dropdownToggle = document.querySelector(
+    ".header__item--has-dropdown .header__dropdowntoggle"
+  );
+  var dropdownMenu = document.querySelector(
+    ".header__item--has-dropdown .header__dropdown"
+  );
   var header = document.querySelector(".header");
 
+  function toggleDropdown() {
+    if (dropdownMenu.classList.contains("show")) {
+      dropdownMenu.classList.remove("show");
+      header.classList.remove("header__expanded");
+    } else {
+      dropdownMenu.classList.add("show");
+      header.classList.add("header__expanded");
+    }
+  }
+
+  dropdownToggle.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      toggleDropdown();
+    }
+  });
+
+  var dropdown = document.querySelector(".header__item--has-dropdown");
+
   dropdown.addEventListener("mouseenter", function () {
+    dropdownMenu.classList.add("show");
     header.classList.add("header__expanded");
   });
 
   dropdown.addEventListener("mouseleave", function () {
+    dropdownMenu.classList.remove("show");
     header.classList.remove("header__expanded");
   });
 });
